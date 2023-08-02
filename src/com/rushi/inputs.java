13dc74847cbafeb4db15f78c4ed27f1decced5e0
Rushikesh <rushikeshnimkar@gmail.com>
2023-08-02 20:16:57 +0530

    inital

---
+++ src/com/rushi/inputs.java
@@ -0,0 +1,14 @@
+package com.rushi;
+
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+
+
+        int n = 200 % 10;
+        System.out.println(np[-[]]);
+
+
+    }
+}
---
+++ src/primitives.java
@@ -0,0 +1,14 @@
+public class primitives {
+    public static void main(String[] args) {
+        int rollno = 21;
+        char name = 'c';
+        float num = 112.324f;
+        double largedecimalnumber = 12313123.34645374567;
+        long largeInteger = 24234532535345L;
+        boolean bool = true;
+
+
+
+
+    }
+}
